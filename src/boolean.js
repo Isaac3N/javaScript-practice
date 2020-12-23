a579f8571da152e10909415c4adecc4a678531a1
Isaac3N <theisaac3n@gmail.com>
2020-12-24 00:59:29 +0100

    updated booleans

--- src/boolean.js
+++ src/boolean.js
@@ -1,2 +1,18 @@
 /* boolean is a data type in javascript that expresses
-a value as either true or false */
+a value as either true or false. Comparison operators are normally used to return/
+an equality operator of true or false
+Comparison operators are:
+== -> equals to(does type conversion i.e 10 == '10')
+=== -> exactly equals to(strict) 
+=! -> not equal(does type conversion) 
+==! -> strict not equal(strict)
+>= -> equals or greater that 
+<= -> less or equals to
+
+To check if two thinga are true at the same time you can you use the '&&'/
+operator. to check if either one of the values are true you use the '||' operator/
+
+the 'else' and 'else if' statement can be used to catch the if statement in case the first condition isnt true 
+
+ 
+ */
