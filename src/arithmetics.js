085c385053a33830068264a9a39fe15925bc287b
Isaac3N <theisaac3n@gmail.com>
2021-01-28 17:32:16 +0100

    arithmetics.js

---
+++ src/arithmetics.js
@@ -0,0 +1,9 @@
+// arithemtic operations in javascripts + - % * /
+
+var addition = 2 + 2;
+var subtraction = 3-73;
+var divison = 10 / 3;
+var multiplication = 10*3;
+var remainder = 10% 3;
+
+console.log(addition);
