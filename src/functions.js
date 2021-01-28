082f455aff5cd7176b55ebd7651c1f5ab22adc84
Isaac3N <theisaac3n@gmail.com>
2021-01-28 17:57:06 +0100

    functions in javascript

---
+++ src/functions.js
@@ -0,0 +1,12 @@
+// functions in javascript is a set of tasks that allow you to carry out a set of tasks
+
+function addNumbers(num1, num2){
+    // function body
+    var result = num1 + num2;
+    return result; 
+};
+var result = (addNumbers(10, 45));
+console.log(result);
+
+console.log(Object.keys({name: 'Isaac', age: 23}));
+console.log(Object.values({name: 'Isaac', age: 23}));
