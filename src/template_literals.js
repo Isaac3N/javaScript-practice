29512f195d3ff55157db9f5ef46c19d78d0f5204
Isaac3N <theisaac3n@gmail.com>
2021-01-29 01:56:37 +0100

    template literals in javascript

---
+++ src/template_literals.js
@@ -0,0 +1,6 @@
+// template literals are used to concatenate strings together using back ticks 
+const name = 'Isaac';
+const country = "Nigeria";
+const age = 33;
+
+console.log(`Name ${name.length} Country ${country} Age ${age}`);
