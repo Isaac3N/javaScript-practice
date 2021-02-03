b42c006d8286d01ccc5bddfd75d5eee526fca6ce
Isaac3N <theisaac3n@gmail.com>
2021-02-03 21:04:50 +0100

    ajaz

--- src/callback.js
+++ src/callback.js
@@ -1,12 +1,36 @@
-function callbackExample(name, callback){
-    console.log(callback(name));
+// function callbackExample(name, callback){
+//     console.log(callback(name));
+// }
+
+// // var callback = function(name){
+// //     return "Hello"+ " " + name; 
+// // }
+// // callbackExample("Isaac Ndubuisi", callback);
+
+// callbackExample("Isaac Ndubuisi", (name)=>{
+//     return `Hello ${name}`
+// })
+
+const posts = [
+    {title: 'Post One', body: 'This is the post one'},
+    {title: 'Post Two', body: 'This is the post two'}
+] // --> to objects in a single array
+
+function getPosts(){
+    setTimeout(()=> {
+        let output = "";
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+         // This lets you look at the HTML markup of the element's content nodes.
+        document.body.innerHtml = output;
+    }, 1000);
+}
+function createPost(post, callback){
+    setTimeout(()=>{
+        posts.push(post);
+        callback();
+    }, 2000);
 }
 
-// var callback = function(name){
-//     return "Hello"+ " " + name; 
-// }
-// callbackExample("Isaac Ndubuisi", callback);
-
-callbackExample("Isaac Ndubuisi", (name)=>{
-    return `Hello ${name}`
-})
+createPost({title: 'Post Three', body: 'This is the post Three'}, getPosts);
